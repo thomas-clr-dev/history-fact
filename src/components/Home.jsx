57e48ac333df32186f2638159c0b0c6256e3ffb4
thomas-clr-dev <thomas.clarisse@my-digital-school.org>
2024-01-16 15:29:19 +0100

    Ajout du style sur la page d'acceuil

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import "../Styles/Home-style.scss";
 const Home = () => {
     return (
         <section>
-            <div>
-                <h1>Quelques Info Sur History-Fact</h1>
-                <div>
-                    <h3>Qu’est ce que c’est ?</h3>
-                    <p>
+            <div id="homeSection">
+                <div className="boxhomeH1">
+                    <h1 id="homeH1">Bienvenue sur History-Fact</h1>
+                </div>
+                <div className="boxQuestion">
+                <div className="h3box"><h3>Qu’est ce que c’est ?</h3></div>
+                    <div className="pQuestionbox"><p>
                         Plongez dans l'histoire chaque jour avec History-Fact,
                         une application web captivante qui vous transporte à
                         travers le temps en révélant les événements historiques
@@ -17,12 +19,12 @@ const Home = () => {
                         cela en un seul clic. Rejoignez-nous pour un voyage
                         fascinant à travers les époques sur la première page de
                         notre site web !
-                    </p>
+                    </p></div>
                 </div>
 
-                <div>
-                    <h3>Comment ça marche ?</h3>
-                    <p>
+                <div className="boxQuestion">
+                    <div className="h3box"><h3>Comment ça marche ?</h3></div>
+                    <div className="pQuestionbox"><p>
                         Découvrez l'histoire comme jamais auparavant en
                         explorant notre page dédiée aux faits historiques sur
                         History-Fact. Chaque heure, plongez dans des événements
@@ -32,7 +34,8 @@ const Home = () => {
                         historiques qui ont marqué le monde. Chaque visite est
                         une immersion unique dans le passé, alors ne manquez pas
                         l'occasion de découvrir l'histoire à chaque clic !
-                    </p>
+                    </p></div>
+                    
                 </div>
             </div>
         </section>
